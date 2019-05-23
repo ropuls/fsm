b31ea265e36fc0abb5adb5642359089eb891e6f5
puls <puls@debian>
2019-05-23 09:41:55 +0200

    updated with solution from so

--- fsm.hpp
+++ fsm.hpp
@@ -2,6 +2,8 @@
 
 #include <variant>
 #include <type_traits>
+#include <cstdio>
+#include "meta.hpp"
 
 /*
  * transition just stores the types used in transitions
@@ -34,12 +36,18 @@ constexpr std::size_t match() {
 } // namespace anon
 
 
-
+template <template <class...> class TT, class ... Ts>
+auto extract_states(TT<Ts...>)
+-> TT<typename Ts::entry_state..., typename Ts::next_state...>;
 
 
 template <typename TransitionTable, typename Context = bool>
 class state_machine {
 public:
+
+    using extracted = decltype(extract_states(std::declval<TransitionTable>()));
+    using states = remove_duplicates_t<extracted>;
+
     static constexpr size_t state_count = std::variant_size_v<TransitionTable>;
     static_assert(state_count > 1, "no state transitions in table");
 
@@ -47,25 +55,26 @@ public:
         m_ctx(c)
     {
         printf("transitions in table: %zd\n", state_count);
+        printf("unique transitions: %zd\n", std::variant_size_v<states>);
     }
 
 
 public:
     template <typename State, typename Event>
-    auto operator()(const State & current_state, Event & event) {
-
+    auto operator()(const State & /*current_state*/, Event & /*event*/) {
+/*
         using state_type            = typename std::decay<State>::type;
         using event_type            = typename std::decay<Event>::type;
         using transition_state_t    = std::variant_alternative_t<match<TransitionTable, state_type, event_type>(), TransitionTable>; // transition_state_t is now a transition<x,y,z>
         using next_state_t          = typename transition_state_t::next_state;
 
-
+*/
         /*
          * instantiate next state
          *
          */
         //next_state_t next(m_ctx);
-        printf("===== state: '%s', event: '%s', next ==> '%12s'\n", typeid(State).name(), typeid(Event).name(), typeid(next_state_t).name());
+  //      printf("===== state: '%s', event: '%s', next ==> '%12s'\n", typeid(State).name(), typeid(Event).name(), typeid(next_state_t).name());
 
 
         /*
